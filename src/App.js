922cc5ceaa01d590af692bfdd31eeae85c244bfc
Anmol Mishra <anmolmishra6568@gmail.com>
2023-07-26 16:34:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import "./App.css"
+import Todolist from './components/TodoList'
+import takingInput from './components/takingInput';
+
+function App() {
+  const [listTodo,setListTodo]=useState([]);
+  let addList = (inputText)=>{
+    if(inputText!=='')
+      setListTodo([...listTodo,inputText]);
+  }
+  const deleteListItem = (key)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
+  return (
+    <div className="main-container">
+      <div className="center-container">
+        <takingInput addList={addList}/>
+        <h1 className="app-heading">TODO</h1>
+        <hr/>
+        {listTodo.map((listItem,i)=>{
+          return (
+            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
